f98bb21ee98f9ebef86c32069aaa2941e12e890e
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2018-07-21 13:11:41 -0300

    Tabela funcionando dando o resultado através de alertas

--- src/pages/tabela/tabela.ts
+++ src/pages/tabela/tabela.ts
@@ -14,12 +14,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'tabela.html',
 })
 export class TabelaPage {
-
+  private qntMes;
+  private hrsDias;
+  private diasSemana;
+  private semanaAno;
+ 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TabelaPage');
+
   }
 
+  /**
+   * 
+   * @param qntMes  quanto o cliente quer ganhar por mes
+   * @param hrsDia  quantas horas quer trabalhar por dia
+   * @param diasSemana  quantos dias quer trabalhar na semana
+   * @param semanaAno  quantas semanas quer trabalhar por ano
+   */
+  calculoFreelancer():number{
+    let valorHoraFinal: number;
+    let valorProjetoFinal:number;
+
+    if(this.hrsDias == null || this.qntMes == null || this.diasSemana == null || this.semanaAno == null){
+      alert("Por favor, preencha todos os dados")
+    }else{
+      let horasPorSemana = this.hrsDias * this.diasSemana;
+      let horasPorAnoDeFolga = horasPorSemana * this.semanaAno;
+      valorHoraFinal = this.qntMes * 12 / (52.1 * horasPorSemana - horasPorAnoDeFolga);
+
+      
+      alert("O seu novo valor por hora é: R$" + valorHoraFinal.toFixed(2) + "/hora");
+      return valorHoraFinal;
+    }
+  }
 }
